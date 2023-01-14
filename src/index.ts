8c1d81cfc194a49b04bda939da359ffdfb286ec2
Adrian <blackrebel.068@gmail.com>
2023-01-14 13:03:07 -0600

    added the arrays

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,14 @@ console.log(typeof []);
 let extintionDino: number = 76_000_000;
 let dinoFavorite: string = "T-Rex";
 let extint = true;
+
+function chanchitoFeliz(config:any) {
+    return config
+}
+
+let animals:string[] = ['chanchito', 'feliz','felipe']
+let nums: number[] = [1,2,3,4,5]
+let checks: boolean[] = []
+let nums2: Array<number> =[]
+
+//nums.map(x => x.) //autocompletado de metodos
